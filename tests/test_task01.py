b2df19aae887b59b06e395250384f43ff953286f
JMQuehl <jannik.quehl@gmail.com>
2023-12-02 22:44:04 +0100

    Solution to Task 1

---
+++ tests/test_task01.py
@@ -0,0 +1,24 @@
+from tests.abstract_test import TaskTest
+import unittest
+from advent_of_code_template.tasks.task01.task01 import Task01
+from advent_of_code_template.advent_of_code_utils import parse_args
+
+
+class Task01Tests(TaskTest, unittest.TestCase):
+    task = Task01(parse_args([]))
+    known_input = ["1abc2\n",
+                   "pqr3stu8vwx\n",
+                   "a1b2c3d4e5f\n",
+                   "treb7uchet"]
+    known_output = 142
+    known_bonus_input = ["two1nine\n",
+                         "eightwothree\n",
+                         "abcone2threexyz\n",
+                         "xtwone3four\n",
+                         "4nineeightseven2\n",
+                         "zoneight234\n",
+                         "7pqrstsixteen"]
+    known_bonus_output = 281
+
+    def test_given_bonus_example(self):
+        assert self.task.solve_bonus_task(self.known_bonus_input) == self.known_bonus_output
