db26ee750f853f817593b2ec6d8d7b0bbae6b2ca
JMQuehl <jannik.quehl@gmail.com>
2023-12-03 14:42:20 +0100

    Solution to Task 3

---
+++ tests/test_task03.py
@@ -0,0 +1,20 @@
+from tests.abstract_test import TaskTest
+import unittest
+from advent_of_code_template.tasks.task03.task03 import Task03
+from advent_of_code_template.advent_of_code_utils import parse_args
+
+
+class Task01Tests(TaskTest, unittest.TestCase):
+    task = Task03(parse_args([]))
+    known_input = ["467..114..\n",
+                   "...*......\n",
+                   "..35..633.\n",
+                   "......#...\n",
+                   "617*......\n",
+                   ".....+.58.\n",
+                   "..592.....\n",
+                   "......755.\n",
+                   "...$.*....\n",
+                   ".664.598.."]
+    known_output = 4361
+    known_bonus_output = 467835
