afb1635de0b6749e29072d58fce330258dcf3c14
JMQuehl <jannik.quehl@gmail.com>
2023-12-10 01:03:26 +0100

    Solution to Task 9

---
+++ tests/test_task09.py
@@ -0,0 +1,13 @@
+from tests.abstract_test import TaskTest
+import unittest
+from advent_of_code_template.tasks.task09.task09 import Task09
+from advent_of_code_template.advent_of_code_utils import parse_args
+
+
+class Task09Tests(TaskTest, unittest.TestCase):
+    task = Task09(parse_args([]))
+    known_input = ["0 3 6 9 12 15\n",
+                   "1 3 6 10 15 21\n",
+                   "10 13 16 21 30 45"]
+    known_output = 114
+    known_bonus_output = 2
